d042dbee8c98da5f7845bfe9f32694461601400a
Mateus Gambaro <magambaro@hotmail.com>
2022-02-05 19:08:45 -0300

    add aws keys

--- aws-node-express-api/handler.js
+++ aws-node-express-api/handler.js
@@ -1,11 +1,17 @@
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
+const { User } = require('../backend/models');
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
+app.get('/', async (_req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
 });
 
 app.get("/hello", (req, res, next) => {
